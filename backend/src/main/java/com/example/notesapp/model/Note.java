33893d5612119788fc2a9796defc835a97fa897d
nitish-11k <nitishk0014@gmail.com>
2025-09-03 19:44:52 +0530

    Fix JPA imports: Change from jakarta to javax for Spring Boot 2.7.18 compatibility

--- backend/src/main/java/com/example/notesapp/model/Note.java
+++ backend/src/main/java/com/example/notesapp/model/Note.java
@@ -4,14 +4,14 @@ import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
-import jakarta.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "notes")
